d5322b957d2bd3c08e57001ebb7ff1105159474b
jayrogers <jannalrgrs@gmail.com>
2017-05-30 13:52:41 -0600

    Created toggle class for Employment History and Education sections, non-displayed in css

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+
+
+
 $(document).ready(function(){
-		$('.one').click(function(){
-			$(this).toggleClass(".changeBackground")
-			.text('clicked');
+		$('.toggle-tlheading').click(function() {
+			if ($(this).next().is(":hidden")) {
+				$(this).next().slideDown("slow");
+			}
+			else {
+				$(this).next().hide();
+			}
 	});
-		//this only applies to what your interacting with on the screen
-		$(".one").mouseenter(function(){
-			$(this).fadeTo("fast",0.25)
-		});
-		$(".one").fadeTo("slow", 1.0);
-			$(this).mouseleave(function(){
-		});
+		
 	});
+ 
+
+
+// $(document).ready(function(){
+// 		$('.one').click(function(){
+// 			$(this).toggleClass(".changeBackground")
+// 			.text('clicked');
+// 	});
+// 		//this only applies to what your interacting with on the screen
+// 		$(".one").mouseenter(function(){
+// 			$(this).fadeTo("fast",0.25)
+// 		});
+// 		$(".one").fadeTo("slow", 1.0);
+// 			$(this).mouseleave(function(){
+// 		});
+// 	});
